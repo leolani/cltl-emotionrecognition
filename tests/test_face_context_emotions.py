22fd34c8dbc7a776d2c4331e7f7a9027e1fba6a8
Thomas Baier <tbaier@math.bme.hu>
2022-10-06 14:28:23 +0200

    Add tests for each test face

--- tests/test_face_context_emotions.py
+++ tests/test_face_context_emotions.py
@@ -3,11 +3,12 @@ import importlib.resources as pkg_resources
 
 import numpy as np
 from PIL import Image
+from parameterized import parameterized
 
 import resources
 import tests.test_data.faces as test_faces
 
-from cltl.emotion_extraction.emotion_mappings import EmotionType
+from cltl.emotion_extraction.emotion_mappings import EmotionType, EkmanEmotion, Sentiment, FaceEmotion
 from cltl.face_emotion_extraction.context_face_emotion_extractor import ContextFaceEmotionExtractor
 
 
@@ -23,8 +24,18 @@ class TestFaceContextEmotions(unittest.TestCase):
             pkg_resources.as_file(model_emotic) as me, pkg_resources.as_file(val_thresholds) as vt:
             self._emotion_extractor = ContextFaceEmotionExtractor(mc, mb, me, vt)
 
-    def test_analyze_text_with_emotion(self):
-        with pkg_resources.open_binary(test_faces, 'anger.png') as image_png:
+    @parameterized.expand([
+        ('anger.png', FaceEmotion.ENGAGEMENT, EkmanEmotion.SURPRISE, Sentiment.POSITIVE),
+        ('cheer.png', FaceEmotion.ENGAGEMENT, EkmanEmotion.SURPRISE, Sentiment.POSITIVE),
+        ('hope.png', FaceEmotion.ENGAGEMENT, EkmanEmotion.SURPRISE, Sentiment.POSITIVE),
+        ('joy.png', FaceEmotion.ENGAGEMENT, EkmanEmotion.SURPRISE, Sentiment.POSITIVE),
+        ('neutral.png', FaceEmotion.ENGAGEMENT, EkmanEmotion.SURPRISE, Sentiment.POSITIVE),
+        ('scared.png', FaceEmotion.ENGAGEMENT, EkmanEmotion.SURPRISE, Sentiment.POSITIVE),
+        ('shocked.png', FaceEmotion.ENGAGEMENT, EkmanEmotion.SURPRISE, Sentiment.POSITIVE),
+        ('smile.png', FaceEmotion.ENGAGEMENT, EkmanEmotion.SURPRISE, Sentiment.POSITIVE),
+    ])
+    def test_analyze_face_emotion(self, image_file, face_emotion, ekman_emotion, sentiment):
+        with pkg_resources.open_binary(test_faces, image_file) as image_png:
             image = np.array(Image.open(image_png))
 
         emotions = self._emotion_extractor.extract_face_emotions(image)
@@ -32,8 +43,8 @@ class TestFaceContextEmotions(unittest.TestCase):
         self.assertEqual(3, len(emotions))
 
         self.assertEqual(EmotionType.FACE, emotions[0].type)
-        self.assertEqual("engagement", emotions[0].value)
+        self.assertEqual(face_emotion.name.lower(), emotions[0].value)
         self.assertEqual(EmotionType.EKMAN, emotions[1].type)
-        self.assertEqual("surprise", emotions[1].value)
+        self.assertEqual(ekman_emotion.name.lower(), emotions[1].value)
         self.assertEqual(EmotionType.SENTIMENT, emotions[2].type)
-        self.assertEqual("positive", emotions[2].value)
+        self.assertEqual(sentiment.name.lower(), emotions[2].value)
