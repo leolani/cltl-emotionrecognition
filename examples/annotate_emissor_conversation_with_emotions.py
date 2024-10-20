2f1896c91b9249b1d11f57b3ac65b337a4a9aa76
piekvossen <piekvossen@gmail.com>
2024-10-20 23:49:12 +0200

    - annotate emissor text.json with emotions from GO

---
+++ examples/annotate_emissor_conversation_with_emotions.py
@@ -0,0 +1,35 @@
+import argparse
+from emissor.persistence import ScenarioStorage
+from emissor.representation.scenario import Modality, Signal
+from cltl.emotion_extraction.add_emotions_to_emissor import EmotionAnnotator
+import pathlib
+import os
+
+def main(emissor: str):
+    annotator = EmotionAnnotator()
+    scenario_folder = emissor
+    scenario_storage = ScenarioStorage(scenario_folder)
+    scenarios = list(scenario_storage.list_scenarios())
+    print("Processing scenarios: ", scenarios)
+    for scenario in scenarios:
+        print('Processing scenario', scenario)
+        scenario_ctrl = scenario_storage.load_scenario(scenario)
+        signals = scenario_ctrl.get_signals(Modality.TEXT)
+        for signal in signals:
+            annotator.process_signal(scenario=scenario_ctrl, signal=signal)
+        #### Save the modified scenario to emissor
+        scenario_storage.save_scenario(scenario_ctrl)
+
+### How to run: python3 examples/annotato_emissor_conversation_with_emotions.py --emissor "../data/emissor"
+
+if __name__ == '__main__':
+    default = "../data/emissor"
+
+    parser = argparse.ArgumentParser(description='Annotate emissor with emotions')
+    parser.add_argument('--emissor', type=str, required=True, help="Path to the folder with emissor scenarios", default=default)
+    args, _ = parser.parse_known_args()
+    folder = os.path.exists(args.emissor)
+    if not os.path.exists(args.emissor):
+        raise ValueError("The folder %s does not exists. The --emissor argument should point to a folder that contains the scenarios to annotate", args.emissor)
+
+    main(args.emissor.strip())
